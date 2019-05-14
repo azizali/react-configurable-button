025df33ec9d1f64b5f94b6afa8d8de1e9973e07d
Aziz Ali <aziz@ilovecoding.org>
2019-05-14 16:55:12 -0700

    PropTypes added to button

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <Button type="submit" value="Click me" style={{ color: 'green' }} />
-      <Button type="button" value="Click me" />
+      <Button type="buttonz" value="Click me" />
       <Button type="anchor" value="Go to google" to="https://google.com" />
     </div>
   );
--- src/Button/index.js
+++ src/Button/index.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export default ({ type, value, to, ...props }) => {
+export default function Button({ type, value, to, ...props }) {
   if (type === 'button') {
     return <button {...props}>{value}</button>
   } else if (type === 'submit') {
     return <input {...props} type="submit" value={value} />
   } else if (type === 'anchor') {
     return <a {...props} href={to}>{value}</a>
+  } else {
+    return null
   }
-  return
 }
+
+Button.propTypes = ({
+  type: PropTypes.oneOf(['button', 'submit', 'anchor']),
+  value: PropTypes.string.isRequired,
+  to: (props, propName, componentName) => {
+    if (props.type === 'anchor' && !props[propName]) {
+      return new Error(`Component ${componentName}: ${propName} is required when prop 'type' has value of 'anchor'`)
+    }
+  }
+})
